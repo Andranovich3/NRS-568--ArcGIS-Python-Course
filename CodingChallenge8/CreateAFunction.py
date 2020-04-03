0ed453a7e23dcfb923b05714ab96f1d85fc7e2bd
Michael Andranovich <60486594+Andranovich3@users.noreply.github.com>
2020-04-03 14:46:36 -0400

    Update CreateAFunction.py

--- CodingChallenge8/CreateAFunction.py
+++ CodingChallenge8/CreateAFunction.py
@@ -1,1 +1,15 @@
-sdfsdd
+# Creating a function from previous code in the semester
+
+# This function will take a list(i) and multiply its contents by a factor(j), then fill and print the new results
+def listMultiplier (i,j):
+  newList = []
+  for x in i:
+    newNumber = x * j
+    newList.append(newNumber)
+  return newList
+
+
+exampleList = [1,2,3,4,5]
+exampleFactor = 10
+
+print listMultiplier(exampleList, ExampleFactor)
